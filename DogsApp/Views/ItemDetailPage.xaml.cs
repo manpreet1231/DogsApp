2ede6a58f65ed036e1e64d5b6c98f88ff165cf3b
manpreet1231 <Aclass.assignments@gmail.com>
2022-02-08 21:40:23 +0500

    This is the DogsApp android application using xamarin. This is the first commit and DogsApp contain built in folders and files.

---
+++ DogsApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DogsApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DogsApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ DogsApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DogsApp.Models;
+using DogsApp.ViewModels;
+using DogsApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DogsApp.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
