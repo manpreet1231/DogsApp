f3dbfa8236d44d9250eb59fc8d5a4076b2ffcaa7
manpreet1231 <Aclass.assignments@gmail.com>
2022-02-08 23:17:57 +0500

    Add a WelcomeViewModel.cs class in ViewModel folder and link bind with WelcomePage.xaml view

---
+++ DogsApp/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using MvvmHelpers;
+using System.Windows.Input;
+
+namespace DogsApp.ViewModels
+{
+    internal class WelcomeViewModel : BaseViewModel
+    {
+        public ICommand OpenGoogleCammand { get; }
+        public WelcomeViewModel() 
+        {
+            Title = "Welcome";
+        }
+    }
+}
